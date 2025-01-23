4f98ed6f9f0cee46f3854865a17c318e1bfc16ad
George Mencoff <george.mencoff@gmail.com>
2025-01-23 10:42:34 -0500

    Update radar tdd setup helper function

--- demos/radar/helpers/radarTddSetup.m
+++ demos/radar/helpers/radarTddSetup.m
@@ -4,8 +4,7 @@ function radarTddSetup(bf_TDD,tpulse,nPulses,tsweep)
 % Copyright 2023 The MathWorks, Inc.
 
 tStartCollection = 0;
-phaserEnable = 1;
-bf_TDD.PhaserEnable = phaserEnable;
+bf_TDD.PhaserEnable = 1;
 bf_TDD.Enable = 0;
 bf_TDD.EnSyncExternal = 1;
 bf_TDD.StartupDelay = 0;
@@ -17,11 +16,11 @@ bf_TDD.Ch0Polarity = 0;
 bf_TDD.Ch0On = 0;
 bf_TDD.Ch0Off = tsweep;
 bf_TDD.Ch1Enable = 1;
-bf_TDD.Ch1Polarity = double(~phaserEnable);
+bf_TDD.Ch1Polarity = 0;
 bf_TDD.Ch1On = tStartCollection;
 bf_TDD.Ch1Off = tStartCollection+0.1;
-bf_TDD.Ch2Enable = 0;
-bf_TDD.Ch2Polarity = double(~phaserEnable);
+bf_TDD.Ch2Enable = 1;
+bf_TDD.Ch2Polarity = 0;
 bf_TDD.Ch2On = 0;
 bf_TDD.Ch2Off = bf_TDD.FrameLength*nPulses;
 bf_TDD.Enable = 1;
